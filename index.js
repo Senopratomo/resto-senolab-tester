371eb7b701fbaf8ac67ac1b0840f1fba7c0cc177
Ernesto Seno <lord.ernesto@gmail.com>
2020-02-28 09:22:22 -0800

    increase interval to 15m

--- index.js
+++ index.js
@@ -8,19 +8,19 @@ async function run() {
     await page.waitFor(2000);
     await page.screenshot({ path: 'screenshots/resto_main.png' });
     await page.goto('https://resto.senolab.org/restaurant.html?id=1');
-    await page.waitFor(1000);
+    await page.waitFor(2000);
     await page.screenshot({path: 'screenshots/resto_1.png'});
     await page.goto('https://resto.senolab.org/restaurant.html?id=2');
-    await page.waitFor(1000);
+    await page.waitFor(2000);
     await page.screenshot({path: 'screenshots/resto_2.png'});
     await page.goto('https://resto.senolab.org/restaurant.html?id=3');
-    await page.waitFor(1000);
+    await page.waitFor(2000);
     await page.screenshot({path: 'screenshots/resto_3.png'});
     await page.goto('https://resto.senolab.org/restaurant.html?id=4');
-    await page.waitFor(1000);
+    await page.waitFor(2000);
     await page.screenshot({path: 'screenshots/resto_4.png'});
     await page.goto('https://resto.senolab.org/restaurant.html?id=5');
-    await page.waitFor(1000);
+    await page.waitFor(2000);
     await page.screenshot({path: 'screenshots/resto_5.png'});
 
     browser.close();
@@ -30,6 +30,6 @@ setInterval(function () {
     let date = new Date();
     console.log(`Running scripts at ${date}`);
     run();
-}, 60000);
+}, 900000);
 
 
